7c24721d682c441cb74db6db9dbeb9e8b92796e4
Johnaits <johnykrc@gmail.com>
2022-07-31 23:32:27 -0300

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-alert("Working");
+$("button").html("<i>Next</i>");
+$("a").attr("href", "https://github.com/Johnaits/JQuery/new/main");
+
+$("h1").click(function()    {
+    $("h1").text("hello!");
+})
+
+$(document).keypress(function(event){
+    $("h1").text((event.key));
+});
+
+$(document).on("mouseover",function(){
+    $("button").css("color","pink");
+})
+
+$("h1").on("click",function(){
+    $("h1").before("<button>Good</button>");
+})
+
+$("h1").on("doubleclick",function(){
+    $("h1").hide;
+})
